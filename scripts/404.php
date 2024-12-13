eef20082a2e4f3f003c47bd59ffaad5fdba1fa43
TecOp <craftland262@gmail.com>
2024-12-13 10:11:29 -0300

    docs: criação pasta scripts

---
+++ scripts/404.php
@@ -0,0 +1,12 @@
+<?php
+defined('CONTROL') or die('Acesso Inválido');
+?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col text-center">
+            <h3 class="text-danger">ERROR 404<br>Recurso não encontrado</h3>
+        </div>
+    </div>
+</div>
+
---
+++ scripts/home.php
@@ -0,0 +1,12 @@
+<?php
+defined('CONTROL') or die('Acesso Inválido');
+?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col text-center">
+            <h3>Vamos consumir uma API com PHP puro</h3>
+        </div>
+    </div>
+</div>
+
